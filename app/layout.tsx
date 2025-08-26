61ac996df2e492d6e97dcc246b3ffc8a27399583
Patryk Dziemianowski <patryk.dziemianowski@gmail.com>
2025-08-26 18:40:36 +0200

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppLogo } from "@/components/app-logo";
 import { Header } from "@/components/layout/header";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gradient-to-br from-stone-100 via-neutral-50 to-amber-50`}
       >
+        <Analytics />
         <Header />
         <AppLogo />
         {children}
